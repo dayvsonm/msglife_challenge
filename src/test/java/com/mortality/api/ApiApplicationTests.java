5e27283f879b17b7e9de80370fe8662a4fc8e6c3
Dayvson Merçon <dayvson.mercon@gmail.com>
2024-07-06 12:02:59 +0100

    feat: first commit

---
+++ src/test/java/com/mortality/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mortality.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
