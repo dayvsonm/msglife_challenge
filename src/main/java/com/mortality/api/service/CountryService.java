6e1b662f5ee1442dd50a6f2bb13b0f9fd6f3aafd
Dayvson Merçon <dayvson.mercon@gmail.com>
2024-07-06 20:06:31 +0100

    feat: Create Junit Teste for Countries and create a Interface for CountrieService

--- src/main/java/com/mortality/api/service/CountryService.java
+++ src/main/java/com/mortality/api/service/CountryService.java
@@ -1,29 +1,10 @@
 package com.mortality.api.service;
 
 import com.mortality.api.domain.country.Country;
-import com.mortality.api.repositories.CountryRepository;
-import jakarta.persistence.NoResultException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-public class CountryService {
-
-    @Autowired
-    private CountryRepository countryRepository;
-
-    public CountryService(CountryRepository countryRepository) {
-        this.countryRepository = countryRepository;
-    }
-
-    public List<Country> getAllCountries() {
-        try {
-            return countryRepository.findAll();
-        }catch (NoResultException ex){
-            throw new NoResultException(ex.getMessage());
-        }
-
-    }
+public interface CountryService {
+    List<Country> getCountries();
+    Country getCountryByCode(String code);
 }
---
+++ src/main/java/com/mortality/api/service/impl/CountryServiceImpl.java
@@ -0,0 +1,35 @@
+package com.mortality.api.service.impl;
+
+import com.mortality.api.domain.country.Country;
+import com.mortality.api.repositories.CountryRepository;
+import com.mortality.api.service.CountryService;
+import jakarta.persistence.NoResultException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CountryServiceImpl implements CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    public CountryServiceImpl(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
+
+    public List<Country> getCountries() {
+        try {
+            return countryRepository.findAll();
+        }catch (NoResultException ex){
+            throw new NoResultException(ex.getMessage());
+        }
+    }
+
+    public Country getCountryByCode(String code) {
+        Optional<Country> countryOptional = countryRepository.findById(code.toUpperCase());
+        return countryOptional.orElseThrow(() -> new NoResultException("País com ISO code '" + code + "' não encontrado."));
+    }
+}
